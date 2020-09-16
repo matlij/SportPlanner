3e959dfaacdfa6fbfd17caad8250f497c7abe02e
Mattias Lij <mattiaslij@gmail.com>
2020-09-16 20:12:57 +0200

    Fixed entry fields in AddPage

--- SportPlanner/SportPlanner/ViewModels/NewItemViewModel.cs
+++ SportPlanner/SportPlanner/ViewModels/NewItemViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using SportPlanner.Models;
@@ -9,8 +11,10 @@ namespace SportPlanner.ViewModels
 {
     public class NewItemViewModel : BaseViewModel
     {
+        private ObservableCollection<EventType> eventTypes = new ObservableCollection<EventType>();
         private EventType eventType;
         private DateTime date;
+
         public string Id { get; set; }
 
         public NewItemViewModel()
@@ -19,14 +23,32 @@ namespace SportPlanner.ViewModels
             CancelCommand = new Command(OnCancel);
             this.PropertyChanged +=
                 (_, __) => SaveCommand.ChangeCanExecute();
+
+            Date = DateTime.Now;
+            PopulateEventTypes();
+        }
+
+        private void PopulateEventTypes()
+        {
+            var eventTypes = Enum.GetValues(typeof(EventType)).Cast<EventType>().Skip(1);
+            foreach (var @event in eventTypes)
+            {
+                EventTypes.Add(@event);
+            }
         }
 
         private bool ValidateSave()
         {
-            return 
-                date != null && 
-                date != DateTime.MinValue && 
+            return
+                date != null &&
+                date != DateTime.MinValue &&
                 eventType != EventType.Undefined;
+        }
+
+        public ObservableCollection<EventType> EventTypes
+        {
+            get => eventTypes;
+            set => SetProperty(ref eventTypes, value);
         }
 
         public EventType EventType
@@ -39,6 +61,16 @@ namespace SportPlanner.ViewModels
         {
             get => date;
             set => SetProperty(ref date, value);
+        }
+
+        public DateTime MinDate
+        {
+            get => DateTime.Now;
+        }
+
+        public DateTime MaxDate
+        {
+            get => DateTime.Now.AddDays(365);
         }
 
         public Command SaveCommand { get; }
