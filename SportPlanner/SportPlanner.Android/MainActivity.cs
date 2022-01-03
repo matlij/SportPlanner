c8611a4ac18aa4acad236bba11cc7549b971037c
Mattias Lij <mattias.lij@teliacompany.com>
2022-01-03 20:14:59 +0100

    Splash sceen Android

--- SportPlanner/SportPlanner.Android/MainActivity.cs
+++ SportPlanner/SportPlanner.Android/MainActivity.cs
@@ -1,15 +1,12 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace SportPlanner.Droid
 {
-    [Activity(Label = "Bandydos", Icon = "@mipmap/bandydos_android_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Bandydos", Icon = "@mipmap/bandydos_android_icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
